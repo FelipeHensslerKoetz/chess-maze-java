db6cd1f1f55980061da0785894d02735a104345f
Felipe Henssler Koetz <felipehenssler@gmail.com>
2019-06-11 12:44:48 -0300

    Painting tiles on the board

--- src/GUI/Table.java
+++ src/GUI/Table.java
@@ -4,24 +4,35 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Table {
 
     private final JFrame gameframe;
-    private final Dimension OUTER_FRAME_DIMENSION = new Dimension(600,600);
+    private final BoardPanel boardPanel;
+    private final static Dimension OUTER_FRAME_DIMENSION = new Dimension(600, 600);
+    private final static Dimension BOARD_PANEL_DIMENSION = new Dimension(400, 35);
+    private final static Dimension TILE_PANEL_DIMENSION = new Dimension(10, 10);
+    private final Color lightTileColor = Color.decode("#FFFACD");
+    private final Color darkTileColor = Color.decode("#593E1A");
 
-
-    public Table(){
-        this.gameframe = new JFrame("ChessMaze");
-        final JMenuBar tableMenuBar = new JMenuBar();
-        populateMenuBar(tableMenuBar);
+    public Table() {
+        this.gameframe = new JFrame("ChessMaze!");
+        this.gameframe.setLayout(new BorderLayout());
+        final JMenuBar tableMenuBar = createTableMenuBar();
         this.gameframe.setJMenuBar(tableMenuBar);
-        this.gameframe.setVisible(true);
+
         this.gameframe.setSize(OUTER_FRAME_DIMENSION);
+        this.boardPanel = new BoardPanel();
+        this.gameframe.add(this.boardPanel, BorderLayout.CENTER);
+        this.gameframe.setVisible(true);
     }
 
-    private void populateMenuBar(JMenuBar tableMenuBar) {
+    private JMenuBar createTableMenuBar() {
+        final JMenuBar tableMenuBar = new JMenuBar();
         tableMenuBar.add(createFileMenu());
+        return tableMenuBar;
     }
 
     private JMenu createFileMenu() {
@@ -34,7 +45,61 @@ public class Table {
             }
         });
         fileMenu.add(openPGN);
+        final JMenuItem exitMenuItem = new JMenuItem("Exit");
+        exitMenuItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        fileMenu.add(exitMenuItem);
         return fileMenu;
+    }
+
+    private class BoardPanel extends JPanel {
+        final List<TilePanel> boardTiles;
+
+        BoardPanel() {
+            super(new GridLayout(8, 8));
+            this.boardTiles = new ArrayList<>();
+            for (int i = 0; i < 64; i++) {
+                final TilePanel tilePanel = new TilePanel(this, i);
+                this.boardTiles.add(tilePanel);
+                add(tilePanel);
+            }
+            setPreferredSize(BOARD_PANEL_DIMENSION);
+            validate();
+        }
+
+    }
+
+    private class TilePanel extends JPanel {
+        private final int tileId;
+
+        TilePanel(final BoardPanel boardPanel, final int tileId) {
+            super(new GridBagLayout());
+            this.tileId = tileId;
+            setPreferredSize(TILE_PANEL_DIMENSION);
+            assignTileColor();
+            validate();
+        }
+
+        private void assignTileColor() {
+            if ((tileId >= 0 && tileId <= 7) ||
+                    (tileId >= 16 && tileId <= 23) ||
+                    (tileId >= 32 && tileId <= 39) ||
+                    (tileId >= 48 && tileId <= 55)
+            ) {
+                setBackground(this.tileId % 2 == 0 ? lightTileColor : darkTileColor);
+            } else if ((tileId >= 8 && tileId <= 15 ||
+                    (tileId >= 24 && tileId <= 31) ||
+                    (tileId >= 40 && tileId <= 47) ||
+                    tileId >= 56 && tileId <= 63)) {
+                setBackground(this.tileId % 2 != 0 ? lightTileColor : darkTileColor);
+            }
+
+        }
+
     }
 
 }
